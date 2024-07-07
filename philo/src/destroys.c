d3d6d0816df37df8789d7580d67cfa9df7d8ff74
amirkhanyan11 <artyom.amirkhanyan11@gmail.com>
2024-07-07 21:12:15 +0400

    a

---
+++ philo/src/destroys.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   destroys.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aamirkha <aamirkha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/07 21:10:53 by aamirkha          #+#    #+#             */
+/*   Updated: 2024/07/07 21:12:07 by aamirkha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philosophers.h"
+
+void fork_destroy(t_fork *fork)
+{
+    
+}
+
+void philo_destroy(t_philo *philo)
+{
+    
+}
+
+void table_destroy(t_terms *table)
+{
+    
+}
